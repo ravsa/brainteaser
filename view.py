7eee384334736d993a98c8cc6a9b229f4818c533
ravsa <rvsbana@gmail.com>
2015-10-12 17:46:31 +0530

    vertical and horizontal boxes added

--- view.py
+++ view.py
@@ -9,9 +9,18 @@ class view():
         self.main_window=gtk.Window()
         self.main_window.connect('destroy',lambda x:gtk.main_quit())
         self.main_window.set_default_size(gtk.gdk.screen_width()-100,gtk.gdk.screen_height()-100)
+        self.main_hbox=gtk.HBox()
+#Three vertical boxes
+        self.vbox_one=gtk.VBox()
+        self.vbox_two=gtk.VBox()
+        self.vbox_three=gtk.VBox()
+#pack three boxes in window into main Horizontal box
+        self.main_window.add(self.main_hbox)
+        self.main_hbox.pack_start(self.vbox_one,False)
+        self.main_hbox.pack_start(self.vbox_two,False)
+        self.main_hbox.pack_start(self.vbox_three,False)
+
         self.main_window.show_all()
         gtk.main()
-
-
 
 y=view()
